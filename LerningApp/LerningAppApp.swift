0a350c29ff59abd58417164048ac62197c2e258f
Brady Robshaw <bradyr8@vt.edu>
2022-01-15 13:16:57 -0500

    Initial Commit

---
+++ LerningApp/LerningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LerningAppApp.swift
+//  LerningApp
+//
+//  Created by Brady Robshaw on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LerningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
